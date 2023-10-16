52c0e7d8cadc95ddb4817ef98f4238233f7aca53
mehdi <mehdi@seald.io>
2023-10-16 08:25:26 +0000

    Pre-derivation

--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -1,2 +1,13 @@
+import scryptJs from 'scrypt-js'
+import { Buffer } from 'buffer'
+
 export const filterWithoutCurrentUser = (users, currentUser) =>
   Object.fromEntries(Object.entries(users).filter(([key]) => key !== currentUser.uid))
+
+export const encode = password => Buffer.from(password.normalize('NFKC'), 'utf8')
+
+export const hashPassword = (password, salt) =>
+  scryptJs.scrypt(encode(password), encode(salt), 16384, 8, 1, 64)
+    // scryptJs returns Uint8Array so we convert it to a proper buffer
+    // to avoid problems
+    .then(res => Buffer.from(res).toString('base64'))
